220980d5dbe2f6e7388dc7e3f2c543e81c17eba4
15683 <15683@mail.ru>
2024-10-02 22:45:01 +0300

    complete first day

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
+)
+
+const (
+	tgBotHost = "api.telegram.org"
 )
 
 func main() {
-	fmt.Println("Hello")
+	tgClient := telegram.New(mustToken())
+
+	// fetcher = fetcher.New(tgClient)
+
+	// processor = processor.New(tgClient)
+
+	// consumer.Start(fetchetm, processor)
+}
+
+func mustToken() string {
+	token := flag.String(
+		"token-bot-token",
+		"",
+		"token for acces to telegram bot",
+	)
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+
+	return *token
 }
