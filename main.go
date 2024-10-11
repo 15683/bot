9f637f181d3aecc713c7799739c311fb74fa1591
15683 <15683@mail.ru>
2024-10-11 13:04:20 +0300

    sqlite3 added

--- main.go
+++ main.go
@@ -1,24 +1,36 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 
 	tgClient "bot/clients/telegram"
 	event_consumer "bot/consumer/event-consumer"
 	"bot/events/telegram"
-	"bot/storage/files"
+	"bot/storage/sqlite"
 )
 
 const (
-	tgBotHost   = "api.telegram.org"
-	storagePath = "files_storage"
-	batchSize   = 100
+	tgBotHost         = "api.telegram.org"
+	sqliteStoragePath = "data/sqlite/storage.db"
+	batchSize         = 100
 )
 
 func main() {
+	// s := files.New(storagePath)
+	s, err := sqlite.New(sqliteStoragePath)
+	if err != nil {
+		log.Fatalf("can't connect to storage: ", err)
+	}
+
+	if err := s.Init(context.TODO()); err != nil {
+		log.Fatalf("can't init storage: ", err)
+	}
+
 	eventsProcessor := telegram.New(
-		tgClient.New(tgBotHost, mustToken()), files.New(storagePath),
+		tgClient.New(tgBotHost, mustToken()),
+		s,
 	)
 
 	log.Print("service started")
